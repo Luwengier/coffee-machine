5844da828f71425818e1d0f088bfdddff758c3c2
Luewngier <g217881111@gmail.com>
2023-08-01 12:24:56 +0800

    refactor app into oop version

--- main.py
+++ main.py
@@ -1,119 +1,38 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
+is_finish = False
 
-COIN_MAPPING = {
-    "quarters": 0.25,
-    "dimes": 0.1,
-    "nickles": 0.05,
-    "pennies": 0.01
-}
-
-isFinish = False
-acc_money = 0
-
-
-def show_report():
-    for resource in resources:
-        unit = 'ml'
-        if resource == 'coffee':
-            unit = 'g'
-        print(f"{resource.capitalize()}: {resources[resource]}{unit}")
-
-    print(f"Money: ${acc_money}")
-
-
-def check_resources(drink: str):
-    """Return True when order can be made, False if ingredients are insufficient."""
-    ingredients = MENU.get(drink).get("ingredients")
-
-    for ingredient in ingredients:
-        if resources.get(ingredient) < ingredients.get(ingredient):
-            print(f"Sorry there is not enough {ingredient}")
-            return False
-    return True
-
-
-def deduct_resources(drink: str):
-    global resources
-    ingredients = MENU.get(drink).get("ingredients")
-    for ingredient in ingredients:
-        resources[ingredient] -= ingredients.get(ingredient)
-
-
-def trade(drink: str):
-    """Return True when the payment is accepted, or False if money is insufficient."""
-    global acc_money
-    cost = MENU.get(drink).get('cost')
-    current = 0
-
-    for coin in COIN_MAPPING:
-        amount = input(f"How many {coin}({COIN_MAPPING.get(coin)})?:")
-        current = round(current + int(amount) * COIN_MAPPING.get(coin), 2)
-        print(current)
-
-    difference = current - cost
-    result = difference >= 0
-
-    if result:
-        acc_money += cost
-    else:
-        print("Sorry that's not enough money. Money refunded.")
-
-    if difference > 0:
-        print(f"Here is ${round(current - cost, 2)} dollars in change.")
-
-    return result
+coffee_maker = CoffeeMaker()
+money_store = MoneyMachine()
+menu = Menu()
 
 
 def start():
-    global isFinish
+    global is_finish
     prompt = input("What would you like? (espresso/latte/cappuccino):")
 
     if prompt == "off":
-        isFinish = True
+        is_finish = True
 
     elif prompt == "report":
-        show_report()
-
-    elif prompt in MENU.keys():
-        if not check_resources(prompt) or not trade(prompt):
-            return
-        deduct_resources(prompt)
-        print(f"Here is your {prompt}. Enjoy!")
+        coffee_maker.report()
+        money_store.report()
 
     else:
-        print("Not valid input")
+        drink = menu.find_drink(prompt)
+
+        if not drink:
+            return
+        if not coffee_maker.is_resource_sufficient(drink):
+            return
+        if not money_store.make_payment(drink.cost):
+            return
+
+        coffee_maker.make_coffee(drink)
 
 
-while not isFinish:
+while not is_finish:
     start()
 
